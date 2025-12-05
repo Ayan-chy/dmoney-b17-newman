1d33a34ec6b7220f12d0a289ba556b55dffdd232
Ayan-chy <ayan.chowdhury34@gmail.com>
2025-12-04 19:39:17 -0500

    Feature/change 1 (#1)
    * Update README and package.json to reflect test script changes
    
    * Refactor README to clarify project purpose, technology stack, and usage instructions
    
    ---------
    
    Co-authored-by: test <test@test.com>

--- report.js
+++ report.js
@@ -2,7 +2,7 @@ const newman = require('newman');
 require('dotenv').config();
  
 newman.run({
-    collection:`https://api.postman.com/collections/37122492-0f1f4a07-365f-46d3-814d-c547d6910833?access_key=${process.env.pmat_key}`,
+    collection:`https://api.postman.com/collections/37122492-0f1f4a07-365f-46d3-814d-c547d6910833?access_key=${process.env.pmat_key}`, 
     //collection:require('./collection/dmoney-user-api-collection.json'),
     reporters: 'htmlextra',
     iterationCount: 1,
@@ -15,3 +15,4 @@ newman.run({
     if (err) { throw err; }
     console.log('collection run complete!');
 });
+ 
